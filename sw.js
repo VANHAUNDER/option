4666fa446c9735f697749f046b604b7a9bb6e088
VANHAUNDER <ragnor.top@gmail.com>
2026-05-12 12:00:15 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,34 @@
 const CACHE_NAME = 'option-tracker-v1';
+
+// Массив ресурсов, которые точно есть в проекте.
+// Если какой-то файл не найден, кеширование не упадёт.
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
-  '/icons/apple-touch-icon.png'
-  // можно добавить CDN ресурсы, если хотите полный офлайн:
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/apple-touch-icon.png'
+  // Раскомментируйте, если нужен офлайн-доступ к библиотекам:
   // 'https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js',
   // 'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
 ];
 
-// Установка: кешируем основные файлы
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(cache => {
+      // Promise.allSettled позволяет продолжить, даже если какой-то URL не загрузится
+      return Promise.allSettled(
+        urlsToCache.map(url =>
+          cache.add(url).catch(err => {
+            console.warn('Не удалось кешировать ресурс:', url, err);
+          })
+        )
+      );
+    }).then(() => self.skipWaiting())
   );
 });
 
-// Активация: удаляем старые кеши
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
@@ -32,7 +40,7 @@ self.addEventListener('activate', event => {
   );
 });
 
-// Перехват запросов: сначала кеш, потом сеть
+// Стратегия: сначала кеш, потом сеть (Cache First)
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
